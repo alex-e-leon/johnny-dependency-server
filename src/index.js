f05312712dc17802b7c82821b21a7a0427ae00a3
Alex Frame <alex.frame@domain.com.au>
2017-06-28 11:34:08 +1000

    feat: Add 404 route.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ app.route('GET', '/', (req, res, ctx) => {
   ctx.send(200, { cute: 'butts' });
 });
 
+app.route('GET', '/favicon.ico', (req, res, ctx) => {
+  ctx.send(404, ' ', { 'Content-Type': 'text/plain' });
+});
+
 app.route('default', (req, res, ctx) => {
   ctx.log.info('Route doesnt exist');
-  ctx.send(404, { message: 'nada butts here' });
+  ctx.send(404, 'Page not found.', { 'Content-Type': 'text/plain' });
 });
 
 app.listen(3000);
