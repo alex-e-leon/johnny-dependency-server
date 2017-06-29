7cf3f6d49a719d1a13ebd4cee70b97f1435ed549
Alex Frame <alex.frame@domain.com.au>
2017-06-29 12:01:03 +1000

    feat: Add cluster management.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 const createApp = require('./app');
+const logger = require('pino')();
 
-const app = createApp();
-app.listen(3000);
+const cluster = require('cluster');
+const numCpus = require('os').cpus().length;
+
+console.log('xxx', process.env.NODE_ENV);
+if (cluster.isMaster && process.env.NODE_ENV === 'production') {
+  logger.info(`Master ${process.pid} is running`);
+
+
+  for (let i = 0; i < numCpus; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (deadWorker, exitCode) => {
+    const deadPID = deadWorker.process.pid;
+
+    if (exitCode === 0) {
+      logger.info(`Gracefully terminated process ${deadPID}.`);
+    } else if (exitCode === 1) {
+      logger.fatal(`Worker ${deadPID} died.`);
+
+      // Restart worker
+      const worker = cluster.fork();
+      const newPID = worker.process.pid;
+
+      logger.info(`New worker ${newPID} started to replace ${deadPID}.`);
+    }
+  });
+} else {
+  const app = createApp();
+  app.listen(3000);
+}
